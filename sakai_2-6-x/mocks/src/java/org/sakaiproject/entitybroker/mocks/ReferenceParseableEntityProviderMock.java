756007a9be7b97b02f6ced172c6bce9a3c1aab6a
david.horwitz@uct.ac.za <david.horwitz@uct.ac.za@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2009-10-10 21:47:33 +0000

    [maven-scm] copy for tag entitybroker-1.3.6
    git-svn-id: https://source.sakaiproject.org/svn/entitybroker/tags/entitybroker-1.3.6@67582 66ffb92e-73f9-0310-93c1-f5514f145a0a

---
+++ sakai_2-6-x/mocks/src/java/org/sakaiproject/entitybroker/mocks/ReferenceParseableEntityProviderMock.java
@@ -0,0 +1,56 @@
+/**********************************************************************************
+ * $URL$
+ * $Id$
+ ***********************************************************************************
+ *
+ * Copyright (c) 2007, 2008 The Sakai Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.osedu.org/licenses/ECL-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ **********************************************************************************/
+
+package org.sakaiproject.entitybroker.mocks;
+
+import org.sakaiproject.entitybroker.EntityReference;
+import org.sakaiproject.entitybroker.entityprovider.CoreEntityProvider;
+import org.sakaiproject.entitybroker.entityprovider.capabilities.ReferenceParseable;
+
+/**
+ * Stub class to make it possible to test the {@link ReferenceParseable} capability, will perform
+ * like the actual class so it can be reliably used for testing
+ * 
+ * @author Aaron Zeckoski (aaronz@vt.edu)
+ */
+public class ReferenceParseableEntityProviderMock extends CoreEntityProviderMock implements
+      CoreEntityProvider, ReferenceParseable {
+
+   /**
+    * TEST Constructor: allows for easy setup of this stub for testing
+    * 
+    * @param prefix
+    * @param ids
+    */
+   public ReferenceParseableEntityProviderMock(String prefix, String[] ids) {
+      super(prefix, ids);
+   }
+
+   /*
+    * (non-Javadoc)
+    * @see org.sakaiproject.entitybroker.entityprovider.capabilities.ReferenceParseable#getParsedExemplar()
+    */
+   public EntityReference getParsedExemplar() {
+      // super simple result for now -AZ
+      return new EntityReference();
+   }
+
+}
